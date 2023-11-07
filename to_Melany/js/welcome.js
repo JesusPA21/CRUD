63ec666708aa9f198b41e00d7cfdfa49eab8a2d4
JesusPA21 <148794461+JesusPA21@users.noreply.github.com>
2023-11-07 10:15:05 -0600

    Add files via upload

---
+++ to_Melany/js/welcome.js
@@ -0,0 +1,12 @@
+/*var texts = ['W', 'E', 'L', 'C', 'O', 'M', 'E',':)'];
+var number_of_particicle=12;
+for (var i =0; i < texts.length; i++)
+
+.background(class="background" + i)
+
+.crterion
+- for (var i=0; i< 10)*/
+
+onload = () =>{
+    document.body.classList.remove("container");
+};
